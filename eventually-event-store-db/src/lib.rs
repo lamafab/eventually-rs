478a2d1d8b4f176b5608907005cfe84a4a22f529
lamafab <42901763+lamafab@users.noreply.github.com>
2020-12-28 23:53:33 +0100

    init simple EventStoreBuilder

--- eventually-event-store-db/src/lib.rs
+++ eventually-event-store-db/src/lib.rs
@@ -1,4 +1,30 @@
+//! EventStoreDB backend implementation for [`eventually` crate](https://crates.io/crates/eventually).
+
+use eventstore::Client as EsClient;
+
 mod store;
 mod stream;
 mod subscriber;
 mod subscription;
+
+/// Builder type for ['EventStore'].
+pub struct EventStoreBuilder {
+    client: EsClient,
+}
+
+impl EventStoreBuilder {
+    /// Create a new builder instance from the connection string.
+    pub async fn new(connection: &str) -> Self {
+        EventStoreBuilder {
+            client: EsClient::create(connection.parse().unwrap()).await.unwrap(),
+        }
+    }
+}
+
+impl From<EsClient> for EventStoreBuilder {
+    fn from(client: EsClient) -> Self {
+        EventStoreBuilder {
+            client: client,
+        }
+    }
+}
