a14033df829fbbe3589dd090a8e2867caf81254f
lamafab <42901763+lamafab@users.noreply.github.com>
2020-12-29 00:17:59 +0100

    init BuilderError

--- eventually-event-store-db/src/lib.rs
+++ eventually-event-store-db/src/lib.rs
@@ -1,11 +1,25 @@
 //! EventStoreDB backend implementation for [`eventually` crate](https://crates.io/crates/eventually).
 
 use eventstore::Client as EsClient;
+use std::error::Error;
 
 mod store;
 mod stream;
 mod subscriber;
 mod subscription;
+
+type Result<T> = std::result::Result<T, BuilderError>;
+
+/// Error type returned by ['EventStoreBuilder'].
+#[derive(Debug, thiserror::Error)]
+pub enum BuilderError {
+    /// Error returned when attempting to parse connection string.
+    #[error("Failed to parse connection string")]
+    InvalidConnection,
+    /// Error returned when attempting to create a gRPC connection to the EventStoreDB databse.
+    #[error("failed to create gRPC client to the EventStoreDB database")]
+    GRpcClient(#[source] Box<dyn Error>),
+}
 
 /// Builder type for ['EventStore'].
 pub struct EventStoreBuilder {
@@ -14,10 +28,12 @@ pub struct EventStoreBuilder {
 
 impl EventStoreBuilder {
     /// Create a new builder instance from the connection string.
-    pub async fn new(connection: &str) -> Self {
-        EventStoreBuilder {
-            client: EsClient::create(connection.parse().unwrap()).await.unwrap(),
-        }
+    pub async fn new(connection: &str) -> Result<Self> {
+        Ok(
+            EventStoreBuilder {
+                client: EsClient::create(connection.parse().map_err(|_| BuilderError::InvalidConnection)?).await.map_err(|err| BuilderError::GRpcClient(err))?,
+            }
+        )
     }
 }
 
