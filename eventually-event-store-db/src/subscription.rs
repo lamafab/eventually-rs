44ed76393e5c8b5951998ad1086028c9cd36424e
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-02 20:07:26 +0100

    implemented resume

--- eventually-event-store-db/src/subscription.rs
+++ eventually-event-store-db/src/subscription.rs
@@ -1,10 +1,14 @@
-use super::store::{process_stream, StoreError};
+use super::store::StoreError;
 use eventstore::PersistentSubscriptionSettings;
-use eventually::subscription::{EventStream, Subscription, SubscriptionStream};
+use eventually::store::Persisted;
+use eventually::subscription::{Subscription, SubscriptionStream};
+use futures::channel::mpsc;
 use futures::future::BoxFuture;
+use futures::stream::StreamExt;
 use serde::de::DeserializeOwned;
 use serde::Serialize;
-use std::fmt::Display;
+use std::convert::TryFrom;
+use std::fmt::{Debug, Display};
 use std::marker::PhantomData;
 
 pub struct EventSubscription<Id, Event> {
@@ -17,15 +21,43 @@ pub struct EventSubscription<Id, Event> {
 
 impl<Id, Event> Subscription for EventSubscription<Id, Event>
 where
-    Id: 'static + Send + Sync + Eq + Display + Clone,
+    Id: 'static + Send + Sync + Eq + Display + TryFrom<String> + Clone,
     Event: 'static + Send + Sync + Serialize + DeserializeOwned,
+    <Id as TryFrom<String>>::Error: Debug,
 {
     type SourceId = Id;
     type Event = Event;
     type Error = StoreError;
 
     fn resume(&self) -> BoxFuture<Result<SubscriptionStream<Self>, Self::Error>> {
-        unimplemented!()
+        let fut = async move {
+            let (mut tx, rx) = mpsc::unbounded();
+
+            self.client
+                .connect_persistent_subscription(self.stream_id, self.subscription_name)
+                .execute()
+                .await
+                .map(|(mut read, _write)| async move {
+                    while let Some(resolved) = read.try_next_event().await.unwrap() {
+                        // TODO: Clarify in what cases `event` might be `None`.
+                        let mut event = resolved.event.unwrap();
+
+                        let stream_id = std::mem::take(&mut event.stream_id);
+                        tx.start_send(Ok(Persisted::from(
+                            Id::try_from(stream_id).unwrap(),
+                            serde_json::from_slice::<Event>(event.data.as_ref()).unwrap(),
+                        )
+                        .version(0)
+                        .sequence_number(0)))
+                            .unwrap();
+                    }
+                })?
+                .await;
+
+            Ok(rx.boxed())
+        };
+
+        Box::pin(fut)
     }
 
     fn checkpoint(&self, version: u32) -> BoxFuture<Result<(), Self::Error>> {
