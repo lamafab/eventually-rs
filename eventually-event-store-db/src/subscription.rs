a54736275357013f61051214d10eec34f219b905
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-02 18:02:33 +0100

    implement boilerplate implemenation of Subscription

--- eventually-event-store-db/src/subscription.rs
+++ eventually-event-store-db/src/subscription.rs
@@ -1,1 +1,27 @@
+use super::store::{process_stream, StoreError};
+use eventually::subscription::{EventStream, Subscription, SubscriptionStream};
+use futures::future::BoxFuture;
+use std::marker::PhantomData;
 
+pub struct EventSubscriber<Id, Event> {
+    client: eventstore::Client,
+    _p1: PhantomData<Id>,
+    _p2: PhantomData<Event>,
+}
+
+impl<Id, Event> Subscription for EventSubscriber<Id, Event>
+where
+    Id: Eq,
+{
+    type SourceId = Id;
+    type Event = Event;
+    type Error = StoreError;
+
+    fn resume(&self) -> BoxFuture<Result<SubscriptionStream<Self>, Self::Error>> {
+        unimplemented!()
+    }
+
+    fn checkpoint(&self, version: u32) -> BoxFuture<Result<(), Self::Error>> {
+        unimplemented!()
+    }
+}
