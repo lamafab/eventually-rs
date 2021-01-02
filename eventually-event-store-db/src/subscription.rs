3fb7559079690d3738592adacf525073453b8081
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-02 18:32:13 +0100

    implement checkpoint method

--- eventually-event-store-db/src/subscription.rs
+++ eventually-event-store-db/src/subscription.rs
@@ -1,17 +1,24 @@
 use super::store::{process_stream, StoreError};
+use eventstore::PersistentSubscriptionSettings;
 use eventually::subscription::{EventStream, Subscription, SubscriptionStream};
 use futures::future::BoxFuture;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::fmt::Display;
 use std::marker::PhantomData;
 
 pub struct EventSubscription<Id, Event> {
     client: eventstore::Client,
+    stream_id: &'static str,
+    subscription_name: &'static str,
     _p1: PhantomData<Id>,
     _p2: PhantomData<Event>,
 }
 
 impl<Id, Event> Subscription for EventSubscription<Id, Event>
 where
-    Id: Eq,
+    Id: 'static + Send + Sync + Eq + Display + Clone,
+    Event: 'static + Send + Sync + Serialize + DeserializeOwned,
 {
     type SourceId = Id;
     type Event = Event;
@@ -22,6 +29,20 @@ where
     }
 
     fn checkpoint(&self, version: u32) -> BoxFuture<Result<(), Self::Error>> {
-        unimplemented!()
+        let fut = async move {
+            self.client
+                .update_persistent_subscription(self.stream_id, self.subscription_name)
+                .execute({
+                    let mut settings = PersistentSubscriptionSettings::default();
+                    settings.revision = version as u64;
+                    // TODO: Verify this:
+                    settings.max_checkpoint_count = 0;
+                    settings
+                })
+                .await
+                .map_err(|err| err.into())
+        };
+
+        Box::pin(fut)
     }
 }
