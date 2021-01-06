fff0aa85b48449e0d7d9b1e88291e08b5fcdef52
lamafab <42901763+lamafab@users.noreply.github.com>
2021-01-06 22:25:01 +0100

    implement Stream for PersistentStream

--- eventually-event-store-db/src/subscription.rs
+++ eventually-event-store-db/src/subscription.rs
@@ -1,14 +1,19 @@
 use super::store::StoreError;
 use super::GenericEvent;
-use eventstore::PersistentSubscriptionSettings;
+use eventstore::prelude::{SubEvent, SubscriptionRead, SubscriptionWrite};
+use eventstore::{PersistentSubscriptionSettings, ResolvedEvent};
 use eventually::store::Persisted;
 use eventually::subscription::{Subscription, SubscriptionStream};
 use futures::channel::mpsc;
 use futures::future::BoxFuture;
-use futures::stream::StreamExt;
+use futures::stream::{Stream, StreamExt};
+use futures::task::{Context, Poll};
+use uuid::Uuid;
 use std::convert::TryFrom;
 use std::fmt::Display;
 use std::marker::PhantomData;
+use std::pin::Pin;
+use std::future::Future;
 
 /// TODO
 pub struct EventSubscription<Id> {
@@ -33,6 +38,72 @@ impl<Id> EventSubscription<Id> {
     }
 }
 
+/// TODO
+pub struct PersistentStream<Id> {
+    reader: SubscriptionRead,
+    writer: SubscriptionWrite,
+    _p1: PhantomData<Id>,
+}
+
+impl<Id> Unpin for PersistentStream<Id> {}
+
+impl<Id> Stream for PersistentStream<Id>
+where
+    Id: TryFrom<String>,
+{
+    type Item = Result<Persisted<Id, GenericEvent>, StoreError>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        fn x<Id>(resolved: ResolvedEvent) -> Result<(Persisted<Id, GenericEvent>, Uuid), StoreError>
+        where
+            Id: TryFrom<String>,
+        {
+            let mut event = resolved.event.unwrap();
+            let uuid = event.id;
+            let stream_id = std::mem::take(&mut event.stream_id);
+
+            Ok((
+                Persisted::from(
+                    Id::try_from(stream_id.clone())
+                        .map_err(|_| StoreError::FailedStreamIdConv(stream_id))?,
+                    GenericEvent::from(event.data),
+                )
+                .version(event.revision as u32)
+                .sequence_number(0),
+                uuid,
+            ))
+        }
+
+        async fn y<Id>(mut stream: Pin<&mut PersistentStream<Id>>) -> Option<Result<Persisted<Id, GenericEvent>, StoreError>>
+        where
+            Id: TryFrom<String>,
+        {
+            // Scope 
+            let event = {
+                if let Some(event) = stream.as_mut().reader.try_next_event().await.unwrap() {
+                    Some(event)
+                } else {
+                    None
+                }
+            };
+
+            if let Some(event) = event {
+                let processed = x::<Id>(event);
+
+                if let Ok((_, uuid)) = processed {
+                    stream.as_mut().writer.ack(vec![uuid]).await.unwrap();
+                }
+
+                Some(processed.map(|(persisted, _)| persisted))
+            } else {
+                None
+            }
+        }
+
+        Box::pin(y(self)).as_mut().poll(cx)
+    }
+}
+
 impl<Id> Subscription for EventSubscription<Id>
 where
     Id: 'static + Send + Sync + Eq + TryFrom<String> + Clone + Display,
@@ -45,8 +116,25 @@ where
         let fut = async move {
             let (mut tx, rx) = mpsc::unbounded();
 
+            // TODO: add trait bound for `AsRef<str>` instead of `Display`
+            /*
             self.client
-                // TODO: add trait bound for `AsRef<str>` instead of `Display`
+                .delete_persistent_subscription(
+                    self.source_id.to_string().as_str(),
+                    self.subscription_name,
+                )
+                .execute().await.unwrap();
+            */
+
+            self.client
+                .create_persistent_subscription(
+                    self.source_id.to_string().as_str(),
+                    self.subscription_name,
+                )
+                .execute(PersistentSubscriptionSettings::default())
+                .await?;
+
+            self.client
                 .connect_persistent_subscription(
                     self.source_id.to_string().as_str(),
                     self.subscription_name,
@@ -54,9 +142,15 @@ where
                 .execute()
                 .await
                 .map(|(mut read, mut write)| async move {
-                    while let Some(resolved) = read.try_next_event().await? {
-                        // TODO: Clarify in what cases `event` might be `None`.
-                        let mut event = resolved.event.unwrap();
+                    while let Some(sub_event) = read.try_next().await? {
+                        println!("LOOP ENTRY");
+
+                        let mut event = match sub_event {
+                            SubEvent::EventAppeared(resolved) => resolved.event.unwrap(),
+                            _ => continue,
+                        };
+
+                        println!("EVENT: {:?}", event);
 
                         let stream_id = std::mem::take(&mut event.stream_id);
                         tx.start_send(Ok(Persisted::from(
@@ -70,6 +164,8 @@ where
 
                         write.ack(vec![event.id]).await.unwrap();
                     }
+
+                    println!("*** *** *** OUT");
 
                     #[allow(unused_qualifications)]
                     Result::<(), StoreError>::Ok(())
--- eventually-event-store-db/tests/subscription.rs
+++ eventually-event-store-db/tests/subscription.rs
@@ -1,10 +1,15 @@
 mod common;
 
 use common::{Event, SourceId};
-use eventually_event_store_db::EventStoreBuilder;
+use eventually::store::{EventStore, Expected};
+use eventually::versioning::Versioned;
+use eventually::Subscription;
+use eventually_event_store_db::{EventStoreBuilder, GenericEvent};
+use futures::{executor, stream::StreamExt};
+use std::collections::HashSet;
 
 #[tokio::test]
-async fn event_store_db_verify_connection_valid() {
+async fn event_store_db_persistant_subscription() {
     let builder = EventStoreBuilder::new("esdb://localhost:2113?tls=false")
         .await
         .unwrap();
@@ -14,5 +19,55 @@ async fn event_store_db_verify_connection_valid() {
         .clone()
         .build_persistant_subscription::<SourceId>(SourceId::Grunt, "test_subscription");
 
+    // Append the expected events.
+    client
+        .append(
+            SourceId::Grunt,
+            Expected::Any,
+            vec![Event::one(), Event::two(), Event::three(), Event::four()],
+        )
+        .await
+        .unwrap();
 
+    println!(">>> >>> SIX");
+    let mut stream = subscription.resume().await.unwrap();
+
+    let mut expected: HashSet<GenericEvent> =
+        [Event::one(), Event::two()].iter().cloned().collect();
+
+    println!(">>> >>> SEVEN");
+    while let Some(event) = stream.next().await {
+        expected.remove(&event.unwrap().take());
+
+        if expected.is_empty() {
+            break;
+        }
+    }
+
+    assert!(expected.is_empty());
+
+    println!(">>> >>> ONE");
+    let mut stream = subscription.resume().await.unwrap();
+
+    let mut expected: HashSet<GenericEvent> =
+        [Event::one(), Event::two()].iter().cloned().collect();
+
+    println!(">>> >>> TWO");
+    let mut at = None;
+    while let Some(event) = stream.next().await {
+        let event = event.unwrap();
+        at = Some(event.version());
+
+        expected.remove(&event.take());
+
+        println!("HIIIII");
+        if expected.is_empty() {
+            break;
+        }
+    }
+
+    println!(">>> >>> THREE");
+    assert!(expected.is_empty());
+
+    subscription.checkpoint(at.unwrap()).await.unwrap();
 }
